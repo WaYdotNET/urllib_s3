9018171e8cb654911ca1695c9cbd33d4c5e965d8
Carlo Bertini <waydotnet@gmail.com>
2018-08-01 11:52:09 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,59 @@
+# coding: utf-8
+
+import os
+import sys
+
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+NAME = "urllib_s3"
+VERSION = '0.0.1'
+
+REQUIRES = [
+    'setuptools >= 21.0.0',
+    'six >= 1.9.0',
+    'minio >= 4.0.2'
+]
+needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
+pytest_runner = ['pytest-runner'] if needs_pytest else []
+setup(
+    name=NAME,
+    version=VERSION,
+    description="S3 handler for urllib",
+    author='Carlo Bertini',
+    author_email="waydotnet@gmail.com",
+    url='https://github.com/WaYdotNET/urllib_s3',
+    license='MIT License',
+    keywords=["urllib", "s3", "urllib handler", 'minio'],
+    package_dir={'': 'lib'},
+    install_requires=REQUIRES,
+    packages=find_packages('lib'),
+    long_description=README,
+
+    classifiers=[
+        'Development Status :: 1 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    setup_requires=pytest_runner,
+    tests_require=[
+        'pytest',
+        'pytest-mock',
+        'pytest-cov',
+        'pytest-flake8',
+        'pytest-isort',
+        'pytest-runner'
+    ],
+
+)
