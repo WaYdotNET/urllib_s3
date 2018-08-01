7852cf5144a7397160baed6f460136436cb3e33c
Carlo Bertini <waydotnet@gmail.com>
2018-08-01 11:51:59 +0200

    test suite

---
+++ test/test_lib.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+import pytest
+from six.moves.urllib.error import URLError
+from six.moves.urllib.request import urlopen
+
+from lib import urllib_s3
+from lib.urllib_s3.error import ServerNameError
+
+
+def test_call_unknown_protocol():
+    with pytest.raises(URLError):
+        urlopen('s3://fake/url.png')
+
+
+def test_missing_setting():
+    with pytest.raises(TypeError):
+        urllib_s3.setup()
+
+
+def test_urllib_s3_with_wrong_credential():
+    urllib_s3.setup({})
+    with pytest.raises(ServerNameError):
+        urlopen('s3://fake/url.png')
+
+
+def test_get_resource(mocker):
+    d = {
+        'fake': {
+            'access_key': 'k',
+            'secret_key': 'k',
+            'secure': False,
+        }
+    }
+    mocker.patch('minio.Minio.get_object', return_value='foo')
+    urllib_s3.setup(d)
+    assert urlopen('s3://fake/url.png') == 'foo'
